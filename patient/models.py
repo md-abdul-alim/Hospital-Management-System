d491e1a27a422318fee0e293505e22214d6dcf1b
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-12-05 21:34:28 +0600

    Update models.py

--- patient/models.py
+++ patient/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Patient(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    user=models.OneToOneField(User,null=True, blank=True,on_delete=models.CASCADE)
     profile_pic= models.ImageField(upload_to='profile_pictures/Patient_Profile_pictures/',null=True,blank=True)
     address = models.CharField(max_length=40)
     mobile = models.CharField(max_length=20,null=False)
